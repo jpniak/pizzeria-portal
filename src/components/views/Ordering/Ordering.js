9186c63042134182c3006ca9e55527e8c64b3e84
Jakub Pniak <jpniak@o2.pl>
2020-07-29 15:00:54 +0200

    Add panel with views

---
+++ src/components/views/Ordering/Ordering.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import styles from './Ordering.scss';
+
+
+const Ordering = () => {
+  return (
+    <div className={styles.component}>
+      <h2>Ordering view</h2>
+      <p>...also called Waiter's view</p>
+      <Link to={process.env.PUBLIC_URL + '/ordering/new'}>New Order</Link>
+      <Link to={process.env.PUBLIC_URL + '/ordering/order/123abc'}>Orders</Link>
+
+    </div>
+  );
+};
+
+export default Ordering;
---
+++ src/components/views/Tables/Tables.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './Tables.scss';
+import { Link } from 'react-router-dom';
+
+const Tables = () => {
+  return (
+    <div className={styles.component}>
+      <h2>Tables view</h2>
+      <Link to={process.env.PUBLIC_URL + '/tables/booking/:id'}>Reservation</Link>
+      <Link to={process.env.PUBLIC_URL + '/tables/event/:id'}>Event</Link>
+    </div>
+  );
+};
+
+export default Tables;
